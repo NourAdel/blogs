92c8f773aba4c3ded019d357b827e57732f28631
Nour Adel <noura7305@gmail.com>
2019-12-03 16:52:30 +0200

    .

---
+++ screens/BlogsIndex.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const BlogsIndex = () => {
+  return <Text>Index</Text>;
+};
+
+const styles = StyleSheet.create({});
+
+export default BlogsIndex;
